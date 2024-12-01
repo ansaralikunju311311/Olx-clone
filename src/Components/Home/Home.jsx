6f672fb8ad1ad5cd2c3d8c3999cfa66ea897854d
ansaralikunju123 <ansarpanoor311@gmail.com>
2024-12-02 02:54:19 +0530

    in this  commit i almost due the ui part of the olx except the product part other ui parts are completly done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+
 import NavBar from '../NavBar/NavBar'
 import Title from '../Title/Title'
 import Banner from '../Banner/Banner'
+import Footer from '../Footer/Footer'
+import BottomFooter from '../BottomFooter/BottomFooter'
 const Home = () => {
   return (
     <div>
       <NavBar />
       <Title />
       <Banner/>
+      <Footer />
+      <BottomFooter/>
     </div>
   )
 }
